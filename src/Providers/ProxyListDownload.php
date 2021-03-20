bbe79d4582ffea8398ec785d75bf3f63e9eca88b
Alex B <alex.belenky@me.com>
2021-03-20 11:13:13 +0200

    FIX proxy-list.download provider

--- src/Providers/ProxyListDownload.php
+++ src/Providers/ProxyListDownload.php
@@ -1,32 +1,32 @@
 <?php namespace ProxyFetcher\Providers;
 
-use Exception;
 use GuzzleHttp\Exception\GuzzleException;
 use ProxyFetcher\Proxy;
+use Symfony\Component\DomCrawler\Crawler;
 
 class ProxyListDownload extends Provider implements ProviderInterface {
-    const URL = 'https://www.proxy-list.download/api/v0/get?l=en&t=';
+    const URL = 'https://www.proxy-list.download/';
 
     /**
      * @return array
      * @throws GuzzleException
      */
     public function fetch(): array {
-        $types  = ['http', 'https', 'socks4', 'socks5'];
+        $types  = ['HTTP', 'HTTPS', 'SOCKS4', 'SOCKS5'];
         $data   = [];
 
         foreach ($types AS $type) {
-            try {
-                foreach (json_decode($this->request(self::URL.$type)->getBody(), true)[0]['LISTA'] AS $row) {
-                    $proxy  = new Proxy();
-                    $proxy->setIp($row['IP']);
-                    $proxy->setPort($row['PORT']);
-                    $proxy->setCountry($row['COUNTRY'] ?? 'N/A');
-                    $proxy->setType($row['ANON']);
+            $this->parse(self::URL.$type, '#tabli > tr')->each(function(Crawler $node) use (&$data) {
+                $tds = $node->filter('td');
 
-                    $data[] = $proxy;
-                }
-            } catch(Exception $e) {}
+                $proxy  = new Proxy();
+                $proxy->setIp($tds->eq(0)->text());
+                $proxy->setPort($tds->eq(1)->text());
+                $proxy->setType($tds->eq(2)->text());
+                $proxy->setCountry($tds->eq(3)->text());
+
+                $data[] = $proxy;
+            });
 
             sleep(2);
         }
